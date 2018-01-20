db73090839d704317f3126954a60e6c7836ad2e2
silvinator <rodewald.mr@gmail.com>
2018-01-20 21:12:55 +0100

    Add files via upload

---
+++ Bot.js
@@ -0,0 +1,168 @@
+var Discord = require('discord.js');
+var Bot = new Discord.Client();
+var Helper = require('./components/helper.js');
+var Queue = require('./components/queue.js');
+var TrackHelper = require('./components/trackhelper.js');
+var WordService = require('./components/wordservice.js');
+var WeatherService = require('./components/weatherservice.js');
+
+var commands = {
+  '!video': {
+    execute: getVideo,
+    description: 'Ich suche ein Video für dich Meister'
+  },
+  '!help': {
+    execute: showHelp
+  },
+  '!play': {
+    execute: doQueue,
+    description: 'queue your song'
+  },
+  '!voteskip': {
+    execute: voteSkip,
+    description: 'vote to skip the current song'
+  },
+  '!song': {
+    execute: showSong,
+    description: 'get the current song'
+  }
+};
+
+Bot.on('message', message => {
+  WordService.registerMessage(message);
+
+  if (isBotCommand(message)) {
+    execute(message.content, message);
+  }
+});
+
+function showSong(args, message) {
+  Queue.showSong(message);
+}
+
+function voteSkip(args, message) {
+  Queue.voteSkip(message);
+}
+
+function doQueue(args, message) {
+  if (args.length <= 0) {
+    return message.reply(Helper.wrap('Welchen typen von music benutzt du Meister,nya? Könntest du mir das sagen, nya?'));
+  }
+
+  if (Queue.isFull()) {
+    return message.reply(Helper.wrap('Entschuldigung Meister, aber ich kann mir dass alles nicht merke, nya...'));
+  }
+
+  if (args.startsWith('http')) {
+    TrackHelper.getVideoFromUrl(args).then(track => {
+      Queue.add(track, message);
+    }).catch(err => {
+      message.reply(Helper.wrap(err));
+    });
+  } else {
+    TrackHelper.getRandomTrack(args, 5).then(track => {
+      Queue.add(track, message);
+    }).catch(err => {
+      message.reply(Helper.wrap(err));
+    });
+  }
+}
+
+function getVideo(args, message) {
+  TrackHelper.getRandomTrack(args, 5).then(track => {
+    message.reply(track.url);
+  }).catch(err => {
+    message.reply(Helper.wrap(err));
+  });
+}
+
+function countWordsByUser(args, message) {
+  WordService.countWordsByUser(args, message);
+}
+
+function getWeather(args, message) {
+  WeatherService.getWeather(args, message);
+}
+
+function showHelp(args, message) {
+  var toReturn = 'Es gibt leider keine Befehle die du ausführen kannst Meister, nya';
+  if (Object.keys(commands).length > 1) {
+    var toReturn = 'Hiehr sind die Befehle Meister, nya:\n';
+    for (var command in commands) {
+      if (command != '!hilfe') {
+        data = commands[command];
+        toReturn += command + ': ' + data.description + getAvailableCommandAsText(data) + '\n';
+      }
+    }
+  }
+  message.reply(Helper.wrap(toReturn));
+}
+
+function getAvailableCommandAsText(command) {
+  if (!Helper.commandIsAvailable(command)) return ' (not available)';
+
+  return '';
+}
+
+function roll(content, message) {
+  message.reply(Helper.wrap('You rolled ' + getRandomNumber(1, 100) + ' (1-100)'));
+}
+
+function isBotCommand(message) {
+  if (message.content.startsWith('!') && message.author.id != Bot.user.id) {
+    return true;
+  }
+
+  return false;
+}
+
+function execute(content, message) {
+  var args = content.split(" ");
+  var command = commands[args[0]];
+  if (command) executeCommand(command, message, args);
+}
+
+function executeCommand(command, message, args) {
+  if (!Helper.commandIsAvailable(command)) {
+    return message.reply(Helper.wrap('Entschuldigung Meister, aber der Befehl ist nicht verfügbar, nya'));
+  }
+
+  command.execute(getCommandArguments(args), message);
+}
+
+function getCommandArguments(args) {
+  var withoutCommand = args.slice(1);
+
+  return withoutCommand.join(" ");
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function registerService(service, affectedCommands) {
+  service = new service();
+
+  if (affectedCommands) {
+    affectedCommands.forEach(command => {
+      var c = commands[command];
+      if (c) {
+        if (!c.services) c.services = [];
+        c.services.push(service);
+      }
+    });
+  }
+
+  return service;
+}
+
+function init() {
+  Helper.keys('apikeys', ['discord']).then(keys => {
+    Bot.login(keys.discord);
+
+    Queue = registerService(Queue, ['!queue', '!voteskip', '!song']);
+    TrackHelper = registerService(TrackHelper, ['!queue', '!video']);
+  }).catch(console.error);
+}
+
+init();
